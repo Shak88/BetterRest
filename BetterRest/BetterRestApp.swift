8f064cbbc4de7277a7ee7032ccea509875ea84e2
Shokri <shokre4@hotmail.com>
2022-04-13 14:22:00 +0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Shokri Alnajjar on 13/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
